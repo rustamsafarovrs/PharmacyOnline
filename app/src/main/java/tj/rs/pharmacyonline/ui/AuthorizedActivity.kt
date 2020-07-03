8092770ea376110332d6993f182dc0286f23d736
rustamsafarovrs <rs.programmer@hotmail.com>
2020-07-03 13:53:31 +0500

    shoving avatar in toolbar

--- app/src/main/java/tj/rs/pharmacyonline/ui/AuthorizedActivity.kt
+++ app/src/main/java/tj/rs/pharmacyonline/ui/AuthorizedActivity.kt
@@ -3,17 +3,27 @@ package tj.rs.pharmacyonline.ui
 import android.os.Bundle
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import kotlinx.android.synthetic.main.toolbar.*
 import tj.rs.pharmacyonline.R
+import tj.rs.pharmacyonline.databinding.ActivityAuthorizedBinding
+import tj.rs.pharmacyonline.ui.profile.profilesettings.ProfileSettingsViewModel
 
 class AuthorizedActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityAuthorizedBinding
+    lateinit var profileViewModel: ProfileSettingsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authorized)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_authorized)
+        binding.lifecycleOwner = this
+        profileViewModel = ViewModelProvider(this).get(ProfileSettingsViewModel::class.java)
+        binding.profileViewModel = profileViewModel
 
         val toolbar = toolbar
         val navController = findNavController(R.id.nav_host_fragment)
@@ -22,5 +32,11 @@ class AuthorizedActivity : AppCompatActivity() {
 
         val profileView = findViewById<RelativeLayout>(R.id.profileRelativeLayout)
         profileView.setOnClickListener { navController.navigate(R.id.profileActivity) }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        profileViewModel.loadProfile()
     }
 }
