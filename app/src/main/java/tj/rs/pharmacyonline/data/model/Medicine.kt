88075a976e952f7d957ccf6ad2d744918455a8cc
rustamsafarovrs <rs.programmer@hotmail.com>
2020-07-06 11:37:40 +0500

    force commit

--- app/src/main/java/tj/rs/pharmacyonline/data/model/Medicine.kt
+++ app/src/main/java/tj/rs/pharmacyonline/data/model/Medicine.kt
@@ -1,5 +1,7 @@
 package tj.rs.pharmacyonline.data.model
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by Rustam Safarov (RS) on 06.04.2020.
  * (c) 2020 RS DevTeam. All rights reserved!
@@ -8,6 +10,7 @@ data class Medicine(
     val id: Int,
     val name: String,
     val price: Int,
+    @SerializedName("description")
     val desc: String,
     val img: String,
     val isFavorite: Boolean = false
